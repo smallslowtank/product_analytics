f1d5428b2fe63639021a6c51b9532323addc16e1
smallslowtank <small.slow.tank@gmail.com>
2025-05-23 12:41:51 +0400

    удалить лишние запросы, оставшиеся поменять местами, поправить описание

--- app/queries.py
+++ app/queries.py
@@ -22,27 +22,9 @@ def select_count():
         return res.scalar()
 
 
-def select_id(n):
-    """
-    Строка по id
-    """
-    with sync_engine.connect() as conn:
-        query = select(YandexImages).where(YandexImages.id == n)
-        return conn.execute(query).first()
-
-
-def get_date(n):
-    """
-    Дата по id
-    """
-    with sync_engine.connect() as conn:
-        query = select(YandexImages.date).where(YandexImages.id == n)
-        return conn.execute(query).scalar()
-
-
 def select_min_date():
     """
-    Ранняя запись
+    Наименьшая дата
     """
     with sync_engine.connect() as conn:
         res = conn.execute(text("SELECT MIN(date) FROM yandex_images"))
@@ -51,54 +33,10 @@ def select_min_date():
 
 def select_max_date():
     """
-    Ранняя запись
+    Наибольшая дата
     """
     with sync_engine.connect() as conn:
         res = conn.execute(text("SELECT MAX(date) FROM yandex_images"))
-        return res.scalar()
-
-
-def select_all_youtube_desktop():
-    """
-    Пример запроса для проверки:
-    Поиск запросов НА ТЕМУ "ютуб" ТОЛЬКО на десктопах
-        SELECT COUNT(*) FROM yandex_images
-        WHERE request LIKE '%ютуб%' OR request LIKE '%Ютуб%'
-        OR request LIKE '%youtu%' OR request LIKE '%Youtu%' OR request LIKE '%YouTu%'
-        GROUP BY platform
-        HAVING platform='desktop'
-    """
-    with sync_engine.connect() as conn:
-        res = conn.execute(
-            text(
-                "SELECT COUNT(*) FROM yandex_images \
-                WHERE request LIKE '%ютуб%' OR request LIKE '%Ютуб%' \
-                OR request LIKE '%youtu%' OR request LIKE '%Youtu%' OR request LIKE '%YouTu%' \
-                GROUP BY platform \
-                HAVING platform='desktop'"
-            )
-        )
-        return res.scalar()
-
-
-def select_only_youtube_touch():
-    """
-    Пример запроса для проверки:
-    Поиск строк, содержащих ТОЛЬКО "ютуб" в запросах ТОЛЬКО на тачах
-        SELECT COUNT(*) FROM yandex_images
-        WHERE request LIKE '%ютуб%'
-        GROUP BY platform
-        HAVING platform='touch'
-    """
-    with sync_engine.connect() as conn:
-        res = conn.execute(
-            text(
-                "SELECT COUNT(*) FROM yandex_images \
-                WHERE request LIKE '%ютуб%' \
-                GROUP BY platform \
-                HAVING platform='touch'"
-            )
-        )
         return res.scalar()
 
 
@@ -124,6 +62,27 @@ def select_only_youtube():
         return res.all()
 
 
+def select_only_youtube_touch():
+    """
+    Пример запроса для проверки:
+    Поиск строк, содержащих ТОЛЬКО "ютуб" в запросах ТОЛЬКО на тачах
+        SELECT COUNT(*) FROM yandex_images
+        WHERE request LIKE '%ютуб%'
+        GROUP BY platform
+        HAVING platform='touch'
+    """
+    with sync_engine.connect() as conn:
+        res = conn.execute(
+            text(
+                "SELECT COUNT(*) FROM yandex_images \
+                WHERE request LIKE '%ютуб%' \
+                GROUP BY platform \
+                HAVING platform='touch'"
+            )
+        )
+        return res.scalar()
+
+
 def select_all_youtube():
     """
     Количество запросов (строка request) НА ТЕМУ "ютуб" на всех платформах
@@ -146,3 +105,26 @@ def select_all_youtube():
             )
         )
         return res.all()
+
+
+def select_all_youtube_desktop():
+    """
+    Пример запроса для проверки:
+    Поиск запросов НА ТЕМУ "ютуб" ТОЛЬКО на десктопах
+        SELECT COUNT(*) FROM yandex_images
+        WHERE request LIKE '%ютуб%' OR request LIKE '%Ютуб%'
+        OR request LIKE '%youtu%' OR request LIKE '%Youtu%' OR request LIKE '%YouTu%'
+        GROUP BY platform
+        HAVING platform='desktop'
+    """
+    with sync_engine.connect() as conn:
+        res = conn.execute(
+            text(
+                "SELECT COUNT(*) FROM yandex_images \
+                WHERE request LIKE '%ютуб%' OR request LIKE '%Ютуб%' \
+                OR request LIKE '%youtu%' OR request LIKE '%Youtu%' OR request LIKE '%YouTu%' \
+                GROUP BY platform \
+                HAVING platform='desktop'"
+            )
+        )
+        return res.scalar()
